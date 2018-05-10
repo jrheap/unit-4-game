00787909d153c0f0b21b9ecb5319d411490da9ac
Jason <jasonrheap@gmial.com>
2018-05-09 23:58:43 -0600

    11:58

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+// Global variables
+var numToGuess = 0;
+var ruby = 0;
+var diamond = 0;
+var emerald = 0;
+var sapphire = 0;
+var win = 0;
+var loss = 0; 
+
+function goal() {
+    var x = Math.floor((Math.random() * 120));
+    document.getElementById("yellow").innerHTML = x;
+};
+
+// each button console.log a random number between 1-12 .on("click")
+function myFunction(){
+console.log(Math.floor(Math.random() * 13));
+
+}
+
+// numToGuess to console.log a random number between 19-120
+    console.log(Math.floor(Math.random() * 120));
+
+// print out num to guess on the in the correct box
+
+
+
+
+
+
+myFunction();
