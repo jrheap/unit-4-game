23815913825f4a285e00b967090ce59f3f949b0c
Jason <jasonrheap@gmial.com>
2018-05-11 17:33:43 -0600

    5:33

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,68 @@
-// Global variables
-var numToGuess = 0;
-var ruby = 0;
-var diamond = 0;
-var emerald = 0;
-var sapphire = 0;
-var win = 0;
-var loss = 0; 
-
-function goal() {
-    var x = Math.floor((Math.random() * 120));
-    document.getElementById("yellow").innerHTML = x;
-};
-
-// each button console.log a random number between 1-12 .on("click")
-function myFunction(){
-console.log(Math.floor(Math.random() * 13));
-
-}
-
-// numToGuess to console.log a random number between 19-120
-    console.log(Math.floor(Math.random() * 120));
-
-// print out num to guess on the in the correct box
+$(document).ready(function() {
 
 
+// we need to print the random number that get put into the goal number
+var goalNum = Math.floor(Math.random()* 120);
+$("#goal").text(goalNum);
+// console.log(goalNum);
+// create variable
+var wins= "";
+var counter = 0;
+var looses = "";
+// make an array of numbers
+var numOptions = [
+    Math.floor(Math.random() * 6) + 3, 
+    Math.floor(Math.random() * 7) + 2, 
+    Math.floor(Math.random() * 5) + 2, 
+    Math.floor(Math.random() * 12) + 4,
+    ];
+
+// console.log(numOptions[0])
+// console.log(numOptions[1])
+// console.log(numOptions[2])
+// console.log(numOptions[3])
+// console.log(numOptions)
+
+// need to give each image a value
+
+
+// click on the picture and print its number on the sceen
 
 
 
 
-myFunction();
+
+
+
+
+
+
+
+
+
+
+  if (counter === goalNum){
+      wins++;
+  } else if (counter > goalNum){
+      looses++;
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
